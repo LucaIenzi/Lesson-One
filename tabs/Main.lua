0a84fa729446ea37b512805d696faa07c2ce9cc3
luca.ienzi@ocsbstudent.ca at codea-scm <luca.ienzi@ocsbstudent.ca@codea-scm.aws.mapote.com>
2015-11-13 10:48:08 -0500

    first comet

---
+++ tabs/Main.lua
@@ -0,0 +1,22 @@
+-- Lesson One
+-- cread
+-- Use this function to perform your initial setup
+print ("hello world")
+-- this program says hello world
+
+    
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(40, 51, 56, 255)
+    fontSize(40)
+    text("hello world", 500, 400)
+
+    -- This sets the line thickness
+    strokeWidth(5)
+
+    -- Do your drawing
+    
+end
+
